92bcd72b79662cbbe9783024503af9a27436277f
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-03-11 18:22:58 -0800

    add Details route

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -37,5 +37,11 @@ namespace HairSalon.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
   }
 }
